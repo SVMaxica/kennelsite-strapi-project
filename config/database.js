bb247ae103fc2750cbc44bfe22b6fe3a7d379138
Sandra Gustafsson <sandragustafsson@Mac-mini.local>
2024-10-08 10:23:44 +0200

    changed settings

--- config/database.js
+++ config/database.js
@@ -2,11 +2,11 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', '127.0.0.1'),
+      host: env('DATABASE_HOST'),
       port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'ventovelocedb'),
-      user: env('DATABASE_USERNAME', 'sandragustafsson'),
-      password: env('DATABASE_PASSWORD', 'Roya_the_saluki!!'),
+      database: env('DATABASE_NAME'),
+      user: env('DATABASE_USERNAME'),
+      password: env('DATABASE_PASSWORD'),
       ssl: env.bool('DATABASE_SSL', false),
     },
     debug: false,
